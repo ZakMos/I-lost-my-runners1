7e6bbc6624003577381083c97632a6f8b932c208
Your Name <you@example.com>
2018-11-21 10:22:27 +0100

    switched code saved on another file

--- js/scene/PlayGame.js
+++ js/scene/PlayGame.js
@@ -2,82 +2,73 @@ class PlayGame extends Phaser.Scene{
   constructor(){
     super({key: "PlayGame"});
   }
-preload () {
-  this.load.image('background', 'assets/background.png', "assets");
-  this.load.image('ground', 'assets/platform.png');
-  this.load.spritesheet('dude','assets/dude.png',
-    { frameWidth: 32, frameHeight: 48 });
-}
-
-// let platforms;
-create () {
-  ///=========================Background=======================///
-
-  let background = this.add.sprite(0, 0, 'background');
-
-  ///=========================Player=======================///
-  let player = this.physics.add.sprite(100, 450, 'dude');
-  console.log(player);
-    this.player.setBounce(0.2);
-    console.log(setBounce);
-    this.player.setCollideWorldBounds(true);
-    this.player.flipX = true;
-    this.player.body.setGravityY(300);
-
-    this.anims.create({
-     key: 'right',
-     frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-     frameRate: 10,
-     repeat: -1
-   }), this;
-
-   ///=========================Ground=======================///
-  let platforms;
-  this.platforms = this.physics.add.staticGroup();
-  this.platforms.create(window.innerWidth/2, window.innerHeight, 'ground').setScale(4).refreshBody();
- }
-
-update () {
-
-  this.physics.add.collider(player, platforms);
-  cursors = this.input.keyboard.createCursorKeys();
-  if (cursors.right.isDown) {
-      this.player.setVelocityX(100);
-      this.player.anims.play('right', true);
+  function preload () {
+     this.load.image('background', 'assets/background.png');
+     this.load.image('ground', 'assets/platform.png');
+     this.load.spritesheet('dude','assets/dude.png',
+      { frameWidth: 32, frameHeight: 48 });
   }
-  if (cursors.space.isDown || cursors.up.isDown && player.body.touching.down)
-  {
-      this.player.setVelocityY(-330);
+  var platforms;
+
+  function create () {
+    ///=========================SKY=======================///
+
+    this.add.image(window.innerWidth/2, window.innerHeight/2, 'background').setScale(2);
+
+    ///=========================Player=======================///
+      player = this.physics.add.sprite(100, 450, 'dude');
+      player.setBounce(0.2);
+      player.setCollideWorldBounds(true);
+      player.flipX = true;
+      player.body.setGravityY(500);
+
+      this.anims.create({
+       key: 'right',
+       frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+       frameRate: 10,
+       repeat: -1
+      });
+     ///=========================Ground=======================///
+
+     platforms = this.physics.add.staticGroup();
+     platforms.create(window.innerWidth/2, window.innerHeight, 'ground').setScale(4).refreshBody();
+
+
+     // Store the score in a variable, initialized at 0
+     this.score = 0;
+
+     // The style of the text
+     let style = { font: '20px Arial', fill: '#fff', backgroundColor: 'black'};
+
+     // Display the score in the top left corner
+     // Parameters: x position, y position, text, style
+     this.scoreText = this.add.text(20, 20, 'score: ' + this.score, style);
+   }
+
+
+
+  function update () {
+    this.physics.add.collider(player, platforms);
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.right.isDown) {
+
+        player.anims.play('right', true);
+        player.x += 3;
+
+    }else if(!cursors.left.isDown){
+        player.anims.play('right', false);
+    }
+    // else if (cursors.right.isDown) {
+    //     player.setVelocityX(160);
+    //     player.anims.play('right', true);
+    // }
+    // else {
+    //     player.setVelocityX(0);
+    //     player.anims.play('turn');
+    // }
+    if (cursors.up.isDown || cursors.space.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+
+    }
   }
-}
-}
-
-
-
-
-
-
-    // mvback = this.add.sprite(0, 400, 'background', './assets/Export/cloud3.png');
-    // mvback.setScale(0.5, 0.5);
-
-    // animation
-    // let frameNames = this.anims.generateFrameNames('background', { start: 1, end: 8, zeroPad: 4, prefix:'background/', suffix:'.png' });
-    // this.anims.create({ key: 'background', frames: frameNames, frameRate: 10, repeat: -1 });
-    // mvback.anims.play('background');
-
-
-
-  // this.add.image(window.innerWidth/2, window.innerHeight/2, 'background').setScale(2.5);
-
-
-
-
-// ===== update ====
-// else if (cursors.right.isDown) {
-//     player.setVelocityX(160);
-//     player.anims.play('right', true);
-// }
-// else {
-//     player.setVelocityX(0);
-//     player.anims.play('turn');
-// }
